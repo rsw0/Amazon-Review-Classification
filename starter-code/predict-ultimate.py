31afdb2c273a01bf873cea57b3f2d59458988dee
Runsheng Wang <runsheng@bu.edu>
2021-03-16 04:41:23 +0800

    correct package import

--- starter-code/predict-ultimate.py
+++ starter-code/predict-ultimate.py
@@ -1,5 +1,9 @@
 import pandas as pd
-import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, confusion_matrix
 
 # try grid search logistic regression with tfidf normalization first
 # Learn the model

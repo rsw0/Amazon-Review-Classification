7662c586d1c656693b2c4b2b6497a56c0d05eb14
Runsheng Wang <runsheng@bu.edu>
2021-03-17 09:03:29 +0800

    ASdasd

--- starter-code/predict-ultimate.py
+++ starter-code/predict-ultimate.py
@@ -19,6 +19,12 @@ Y_validation = pd.read_pickle("./data/Y_validation.pkl")
 X_submission = pd.read_pickle("./data/X_submission.pkl")
 
 
+# Change Type
+print("Changing label type to be categorical")
+Y_train= Y_train.astype('category')
+Y_validation = Y_train.astype('category')
+
+
 # Removing String Columns
 print("Dropping Unused Columns")
 X_train = X_train.drop(columns=['Summary', 'Text'])
@@ -35,7 +41,7 @@ dsubmission = xgb.DMatrix(X_submission)
 
 # Setting Parameters
 print("Setting XGBoost Parameters...")
-param = {'max_depth':2, 'eta':1, 'objective':'multi:softmax', 'num_class': 4}
+param = {'max_depth':2, 'eta':1, 'objective':'multi:softmax', 'num_class': 5}
 num_round = 999
 
 eval_set = [(dvalidation, "Validation")]

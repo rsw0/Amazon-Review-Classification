b1d42282be99247bda95709b6ccde0a7a9666303
Runsheng Wang <runsheng@bu.edu>
2021-03-17 09:46:19 +0800

    a

--- starter-code/predict-ultimate.py
+++ starter-code/predict-ultimate.py
@@ -23,10 +23,8 @@ X_train = X_train.drop(columns=['Summary', 'Text'])
 X_validation = X_validation.drop(columns=['Summary', 'Text'])
 X_submission = X_submission.drop(columns = ['Summary', 'Text'])
 
-#clf = RandomForestClassifier(n_estimators = 1000, min_samples_leaf = 50, n_jobs = -1, oob_score = True, class_weight='balanced', random_state=0)
+clf = RandomForestClassifier(max_depth = 8, n_estimators = 30, min_sample_split = 200, max_samples = 0.2, max_features = 3, min_samples_leaf = 50, n_jobs = -1, oob_score = True, class_weight='balanced_subsample', random_state=0)
 
-base_estimator = AdaBoostClassifier(n_estimators=10)
-clf = EasyEnsembleClassifier(n_estimators=10, base_estimator=base_estimator)
 clf.fit(X_train, Y_train)
 
 Y_validation_predictions = clf.predict(X_validation)

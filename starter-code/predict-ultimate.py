92fa424bd898889821ebea07dd09c72ff6e626ab
Runsheng Wang <runsheng@bu.edu>
2021-03-17 10:18:05 +0800

    a

--- starter-code/predict-ultimate.py
+++ starter-code/predict-ultimate.py
@@ -33,7 +33,8 @@ parameters = [{ 'loss': ['hinge', 'log', 'perceptron'],
                 'alpha': 10.0**-np.arange(1,7),
                 'penalty': ['l1', 'l2', 'elasticnet'],
                 }]
-clf_refined = GridSearchCV(SGDClassifier(random_state=22), parameters, 'n_iter_': [2,3,4,5])
+n_iter_: [2,3,4,5]
+clf_refined = GridSearchCV(SGDClassifier(random_state=22), parameters, n_iter_)
 clf_refined.fit(X_train, Y_train)
 
 clf_SGD_refined.best_params_

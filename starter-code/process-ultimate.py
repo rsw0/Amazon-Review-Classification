f304d8942a545c91e236533a48f944de88d5d1f0
Runsheng Wang <runsheng@bu.edu>
2021-03-17 11:03:49 +0800

    a

--- starter-code/process-ultimate.py
+++ starter-code/process-ultimate.py
@@ -17,6 +17,10 @@ from sklearn.metrics import mean_squared_error, confusion_matrix
 import seaborn as sns
 import matplotlib.pyplot as plt
 import xgboost as xgb
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import mean_squared_error, confusion_matrix
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import GridSearchCV
 
 
 # # Loading
@@ -153,10 +157,36 @@ X_train, X_validation, Y_train, Y_validation = train_test_split(X_train.drop(['S
 
 
 
+# Removing String Columns
+print("Dropping Unused Columns")
+X_train = X_train.drop(columns=['Summary', 'Text'])
+X_validation = X_validation.drop(columns=['Summary', 'Text'])
+X_submission = X_submission.drop(columns = ['Summary', 'Text'])
+
+clf = RandomForestClassifier(n_estimators = 1000, n_jobs = -1, oob_score = True, class_weight='balanced', random_state=0)
+
+clf.fit(X_train, Y_train)
+
+Y_validation_predictions = clf.predict(X_validation)
+X_submission['Score'] = clf.predict(X_submission)
+
+print("RMSE on validation set = ", mean_squared_error(Y_validation, Y_validation_predictions))
+
+# Plot a confusion matrix
+cm = confusion_matrix(Y_validation, Y_validation_predictions, normalize='true')
+sns.heatmap(cm, annot=True)
+plt.title('Confusion matrix of the classifier')
+plt.xlabel('Predicted')
+plt.ylabel('True')
+plt.savefig('matrix.png', dpi=300)
+
+# Create the submission file
+submission = X_submission[['Id', 'Score']]
+submission.to_csv("./data/submission.csv", index=False)
 
 
 
-
+'''
 # Saving to Local
 print("Saving to Local...")
 X_train.to_pickle("./data/X_train.pkl")
@@ -164,7 +194,7 @@ X_validation.to_pickle("./data/X_validation.pkl")
 Y_train.to_pickle("./data/Y_train.pkl")
 Y_validation.to_pickle("./data/Y_validation.pkl")
 X_submission.to_pickle("./data/X_submission.pkl")
-
+'''
 
 
 # Old Functions
